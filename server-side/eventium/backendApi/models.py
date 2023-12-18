5d75601f23f158c80366ad064f8d1e53ebe099a2
Maximus019BG <maxralev@abv.bg>
2023-12-18 18:44:29 +0200

    Revert "Django database setup"
    This reverts commit 28c177147183607be1be9e3354d2f65df1e5ab15.

--- server-side/eventium/backendApi/models.py
+++ server-side/eventium/backendApi/models.py
@@ -1,12 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length=24,default='',unique=True)
-    password = models.CharField(max_length=30,default='')
-
-class Posts(models.Model):
-    name = models.CharField(max_length=50,default='Събитие')
-    description = models.CharField(max_length=250, blank=False,)
-    time = models.DateTimeField(auto_now_add=True)
-
